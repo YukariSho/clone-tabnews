09474d60903656676fe5752e62624257ff482857
YukariSho <86493129+YukariSho@users.noreply.github.com>
2023-11-14 20:16:41 +0000

    add img

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import imagemRick from 'https://i2.wp.com/lojamundogeek.com.br/wp-content/uploads/2023/11/Recapitulacao-do-episodio-5-da-7a-temporada-de-Rick.jpg';
-
 function Home() {
     return (
         <div>
             <h1>Wubba lubba dub dub!</h1>
-            <img src={imagemRick} alt="Descrição da imagem" />
+            <img src="https://i2.wp.com/lojamundogeek.com.br/wp-content/uploads/2023/11/Recapitulacao-do-episodio-5-da-7a-temporada-de-Rick.jpg" alt="R" />
         </div>
     );
 }
