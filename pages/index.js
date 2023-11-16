c952f08c0f35b2218b6520bbb700211dd1e7e35e
YukariSho <86493129+YukariSho@users.noreply.github.com>
2023-11-16 04:17:37 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return (
-        <div>
-            <h1>Wubba lubba dub dub!</h1>
-            <img src="https://i2.wp.com/lojamundogeek.com.br/wp-content/uploads/2023/11/Recapitulacao-do-episodio-5-da-7a-temporada-de-Rick.jpg" alt="R" />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Wubba lubba dub dub!</h1>
+      <img
+        src="https://i2.wp.com/lojamundogeek.com.br/wp-content/uploads/2023/11/Recapitulacao-do-episodio-5-da-7a-temporada-de-Rick.jpg"
+        alt="R"
+      />
+    </div>
+  );
 }
 
 export default Home;
