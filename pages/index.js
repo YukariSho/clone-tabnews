be5135af32aa93d2d2ef1fd20a55a19ea672b2e2
YukariSho <86493129+YukariSho@users.noreply.github.com>
2023-12-08 22:25:04 +0000

    move `.env` to `.env.development`

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div>
       <h1>Wubba lubba dub dub!</h1>
       <img
-        src="https://i2.wp.com/lojamundogeek.com.br/wp-content/uploads/2023/11/Recapitulacao-do-episodio-5-da-7a-temporada-de-Rick.jpg"
+        src="pages/api/v1/status/WhatsApp Image 2023-12-08 at 19.21.43.jpeg"
         alt="R"
       />
     </div>
