720517c06540d17f013f515ef6a0fce17f8e5e97
YukariSho <86493129+YukariSho@users.noreply.github.com>
2023-12-09 00:41:33 +0000

    att img

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>Wubba lubba dub dub!</h1>
+      <h1>DA BOLINHA!</h1>
       <img
-        src="pages/api/v1/status/WhatsApp Image 2023-12-08 at 19.21.43.jpeg"
+        src="https://i.postimg.cc/HnkhtKNn/Whats-App-Image-2023-12-08-at-19-21-43.jpg"
         alt="R"
       />
     </div>
